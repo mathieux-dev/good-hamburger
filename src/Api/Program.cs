69fe4e9cf7faef84460172af4141d9b7a1ac8dd7
mathieux-dev <mateusvmourao@gmail.com>
2026-04-20 17:21:10 -0300

    feat(api): wire up DI, EF Core, and connection strings

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,10 +1,41 @@
+using GoodHamburger.Application.Menu.Handlers;
+using GoodHamburger.Application.Orders.Handlers;
+using GoodHamburger.Domain.Discounts;
+using GoodHamburger.Domain.Interfaces;
+using GoodHamburger.Infrastructure.Persistence;
+using GoodHamburger.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+builder.Services.AddSingleton<IDiscountStrategy, FullComboDiscountStrategy>();
+builder.Services.AddSingleton<IDiscountStrategy, SandwichDrinkDiscountStrategy>();
+builder.Services.AddSingleton<IDiscountStrategy, SandwichSideDiscountStrategy>();
+
+builder.Services.AddScoped<CreateOrderHandler>();
+builder.Services.AddScoped<UpdateOrderHandler>();
+builder.Services.AddScoped<DeleteOrderHandler>();
+builder.Services.AddScoped<GetOrderHandler>();
+builder.Services.AddScoped<GetAllOrdersHandler>();
+builder.Services.AddScoped<GetMenuHandler>();
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 if (app.Environment.IsDevelopment())
 {
