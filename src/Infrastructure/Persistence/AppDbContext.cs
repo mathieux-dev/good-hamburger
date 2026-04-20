813de7a27df9223cdf4fd92bffee355e40fb7816
mathieux-dev <mateusvmourao@gmail.com>
2026-04-20 17:18:09 -0300

    feat(infra): add AppDbContext with entity configuration and menu seed data

---
+++ src/Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,55 @@
+using GoodHamburger.Domain.Entities;
+using GoodHamburger.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoodHamburger.Infrastructure.Persistence;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Order> Orders => Set<Order>();
+    public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+    public DbSet<Product> Products => Set<Product>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(e =>
+        {
+            e.HasKey(p => p.Id);
+            e.Property(p => p.Name).IsRequired().HasMaxLength(100);
+            e.Property(p => p.Price).HasPrecision(10, 2);
+            e.Property(p => p.Category).HasConversion<string>();
+        });
+
+        modelBuilder.Entity<Order>(e =>
+        {
+            e.HasKey(o => o.Id);
+            e.Property(o => o.Subtotal).HasPrecision(10, 2);
+            e.Property(o => o.Discount).HasPrecision(10, 2);
+            e.Property(o => o.Total).HasPrecision(10, 2);
+            e.HasMany(o => o.Items)
+             .WithOne()
+             .HasForeignKey(i => i.OrderId)
+             .OnDelete(DeleteBehavior.Cascade);
+        });
+
+        modelBuilder.Entity<OrderItem>(e =>
+        {
+            e.HasKey(i => i.Id);
+            e.Property(i => i.UnitPrice).HasPrecision(10, 2);
+            e.HasOne(i => i.Product)
+             .WithMany()
+             .HasForeignKey(i => i.ProductId)
+             .OnDelete(DeleteBehavior.Restrict);
+        });
+
+        modelBuilder.Entity<Product>().HasData(
+            new Product(Guid.Parse("11111111-0000-0000-0000-000000000001"), "X Burger",    5.00m, ProductCategory.Sandwich),
+            new Product(Guid.Parse("11111111-0000-0000-0000-000000000002"), "X Egg",       4.50m, ProductCategory.Sandwich),
+            new Product(Guid.Parse("11111111-0000-0000-0000-000000000003"), "X Bacon",     7.00m, ProductCategory.Sandwich),
+            new Product(Guid.Parse("11111111-0000-0000-0000-000000000004"), "Batata Frita",2.00m, ProductCategory.Side),
+            new Product(Guid.Parse("11111111-0000-0000-0000-000000000005"), "Refrigerante",2.50m, ProductCategory.Drink)
+        );
+    }
+}
