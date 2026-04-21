48a46a06d0b11f3e3a0092d569b1b96c9d2d3eb7
mathieux-dev <mateusvmourao@gmail.com>
2026-04-21 00:07:34 -0300

    refactor: simplify GetMenuAsync method by removing caching logic

--- src/Web/Services/MenuService.cs
+++ src/Web/Services/MenuService.cs
@@ -10,13 +10,8 @@ public interface IMenuService
 public class MenuService : IMenuService
 {
     private readonly ApiClient _api;
-    private List<MenuItemDto>? _cache;
-
     public MenuService(ApiClient api) => _api = api;
 
     public async Task<List<MenuItemDto>> GetMenuAsync(CancellationToken ct = default)
-    {
-        _cache ??= await _api.GetMenuAsync(ct);
-        return _cache;
-    }
+        => await _api.GetMenuAsync(ct);
 }
